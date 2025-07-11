47f1d0fec34bea4a8548051936bc10d7db709426
atto08 <atto08@naver.com>
2025-07-11 14:43:19 +0900

    initial commit

---
+++ src/test/java/com/project/Karman/KarmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Karman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
