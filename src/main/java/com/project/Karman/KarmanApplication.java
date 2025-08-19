3ce75f551021394ca9bfbd3622ef3a6c8a300640
atto08 <atto08@naver.com>
2025-08-19 15:16:38 +0900

    initial commit

---
+++ src/main/java/com/project/Karman/KarmanApplication.java
@@ -0,0 +1,13 @@
+package com.project.Karman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarmanApplication.class, args);
+	}
+
+}
