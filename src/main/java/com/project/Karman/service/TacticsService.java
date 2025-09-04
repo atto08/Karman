a4796b84d8b86c9564dc6accf21ef1a4ba9958de
atto08 <atto08@naver.com>
2025-09-04 23:22:36 +0900

    프롬프트 메시지 User, System 메시지 수정

--- src/main/java/com/project/Karman/service/TacticsService.java
+++ src/main/java/com/project/Karman/service/TacticsService.java
@@ -28,26 +28,22 @@ public class TacticsService {
     @Transactional
     public String askTacticalCoach(AskTacticsRequest request) {
 
-        // TODO: 추출 키워드 토대로 유사도 검색하는 방향으로 개선
         // 1) 키워드 추출
         // 프롬프트 생성 - 키워드 추출
         Prompt keywordPrompt = openAiService.createPrompt(PromptMessage.EXTRACT_KEYWORD_SYSTEM, PromptMessage.EXTRACT_KEYWORD_USER, request.ask(), null);
         // Ai 응답 생성 - 키워드 추출
         String formation = openAiService.askChatModel(keywordPrompt).getResult().getOutput().getText();
-        log.info("--extract formation--{}", formation);
+        log.info("---formation: {} & subtopic: {}", formation, request.subtopic());
 
         // 2) 유사도 검색 - metadata로 범위 축소
         List<Document> documents = documentService.similaritySearch(request.ask(), request.subtopic(), formation);
-        log.info("유사도 검색 결과:{}", documents.size());
-
-        // 검색 결과 통합
+        log.info("---유사도 검색 결과: {}", documents.size());
+        // 유사도 검색 결과 통합 - context
         StringBuilder context = new StringBuilder();
-        log.info("Start-- print Document Text");
         for (Document document : documents) {
-            log.info(document.getText());
+            log.info("---문서정보: {}",document.getText());
             context.append(document.getText()).append("\n");
         }
-        log.info("Finish-- print Document Text");
 
         // 프롬프트 생성 - 사용자 요청
         Prompt prompt = openAiService.createPrompt(PromptMessage.ASK_TACTICS_SYSTEM, PromptMessage.ASK_TACTICS_USER, request.ask(), context.toString());
