a03ebca86d483f2d6a67869997c93133f216486d
atto08 <atto08@naver.com>
2025-09-04 23:23:14 +0900

    선수 포지션 세분화(포지션 추가)

--- src/main/java/com/project/Karman/domain/enums/Position.java
+++ src/main/java/com/project/Karman/domain/enums/Position.java
@@ -5,8 +5,16 @@ import lombok.Getter;
 @Getter
 public enum Position {
     FW("공격수"),
+    CF("중앙 공격수"),
+    WF("윙어"),
     MF("미드필더"),
+    CM("중앙 미드필더"),
+    CAM("공격형 미드필더"),
+    CDM("수비형 미드필더"),
     DF("수비수"),
+    WB("윙백"),
+    FB("풀백"),
+    CB("센터백"),
     GK("골키퍼");
 
     private final String description;
