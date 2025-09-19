29be786de410235c89a138506dc84c1f1bf79bce
atto08 <atto08@naver.com>
2025-09-19 16:29:59 +0900

    [fix] EnableJpaAuditing 추가

--- src/main/java/com/project/Karman/KarmanApplication.java
+++ src/main/java/com/project/Karman/KarmanApplication.java
@@ -2,8 +2,10 @@ package com.project.Karman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class KarmanApplication {
 
 	public static void main(String[] args) {
