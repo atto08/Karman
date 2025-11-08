47fdb3d77a71baa4626ad1544e11c647ac2bb8a4
atto08 <atto08@naver.com>
2025-11-08 23:39:03 +0900

    [feat] requestDto 유효성 검사 예외 핸들러 추가

--- src/main/java/com/project/Karman/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/Karman/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.project.Karman.exception;
 import com.project.Karman.dto.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -26,5 +28,24 @@ public class GlobalExceptionHandler {
         log.error(e.getMessage(), e);
         return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(ErrorResponse.error(SYSTEM_ERROR.getMessage(),
                 SYSTEM_ERROR.getCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleNotValidException(MethodArgumentNotValidException e) {
+
+        String errorMessage = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String message = error.getDefaultMessage();
+                    return fieldName + ": " + message;
+                })
+                .reduce((msg1, msg2) -> msg1 + ", " + msg2)
+                .orElse("유효성 검증에 실패했습니다.");
+
+        log.warn("Validation failed: {}", errorMessage);
+
+        return ResponseEntity.badRequest().body(ErrorResponse.error(errorMessage, 400));
     }
 }
